4445d9668ba0d18b0575eec24ba569f00d9b48c5
Aaron Kromer <aaron.kromer@gmail.com>
2014-07-31 12:06:23 -0400

    Use new AltBeacon version of ProximityKit lib.

--- AndroidProximityKitReference/src/main/java/com/radiusnetworks/androidproximitykitreference/AndroidProximityKitReferenceApplication.java
+++ AndroidProximityKitReference/src/main/java/com/radiusnetworks/androidproximitykitreference/AndroidProximityKitReferenceApplication.java
@@ -7,12 +7,13 @@ import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
-import com.radiusnetworks.ibeacon.Region;
 import com.radiusnetworks.proximity.ProximityKitManager;
 import com.radiusnetworks.proximity.ProximityKitNotifier;
-import com.radiusnetworks.ibeacon.IBeacon;
-import com.radiusnetworks.ibeacon.IBeaconData;
-import com.radiusnetworks.ibeacon.client.DataProviderException;
+
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconData;
+import org.altbeacon.beacon.Region;
+import org.altbeacon.beacon.client.DataProviderException;
 
 import java.util.Date;
 
@@ -20,18 +21,19 @@ import java.util.Date;
  * Created by dyoung on 4/2/14.
  */
 public class AndroidProximityKitReferenceApplication extends Application implements ProximityKitNotifier {
+
     public static final String TAG = "ProximityKitReferenceApplication";
-    private boolean haveDetectedIBeaconsSinceBoot = false;
-    private MainActivity mainActivity = null;
     Date lastRefreshTime = new Date();
+    private boolean haveDetectedBeaconsSinceBoot = false;
+    private MainActivity mainActivity = null;
 
     @Override
     public void onCreate() {
         super.onCreate();
+
         ProximityKitManager pkManager = ProximityKitManager.getInstanceForApplication(this);
         pkManager.setNotifier(this);
         pkManager.start();
-
     }
 
     @Override
@@ -45,34 +47,40 @@ public class AndroidProximityKitReferenceApplication extends Application impleme
     }
 
     @Override
-    public void iBeaconDataUpdate(IBeacon iBeacon, IBeaconData iBeaconData, DataProviderException e) {
+    public void beaconDataUpdate(Beacon beacon, BeaconData beaconData, DataProviderException e) {
         if (e != null) {
             Log.d(TAG, "data fetch error:" + e);
         }
-        if (iBeaconData != null) {
-            Log.d(TAG, "I have an iBeacon with data: uuid="+iBeacon.getProximityUuid()+" major="+iBeacon.getMajor()+" minor="+iBeacon.getMinor()+" welcomeMessage="+iBeaconData.get("welcomeMessage"));
-            String displayString = iBeacon.getProximityUuid()+" "+iBeacon.getMajor()+" "+iBeacon.getMinor()+"\n"+"Welcome message:"+iBeaconData.get("welcomeMessage");
+        if (beaconData != null) {
+            Log.d(
+                    TAG,
+                    "I have an beacon with data: " + beacon + " welcomeMessage=" +
+                            beaconData.get("welcomeMessage")
+            );
+            // You could instead call beacon.toString() which wraps up the identifiers
+            String displayString = beacon.getId1() + " " +
+                    beacon.getId2().toInt() + " " + beacon.getId3().toInt() +
+                    "\nWelcome message: " + beaconData.get("welcomeMessage");
             if (mainActivity != null) {
-                mainActivity.displayTableRow(iBeacon, displayString, true);
+                mainActivity.displayTableRow(beacon, displayString, true);
             }
-            if (new Date().getTime() - lastRefreshTime.getTime() > 24*60*60000l) { /* one day */
-                Log.d(TAG, "It is time to force a refresh of iBeacon data");
-                iBeaconData.sync(this);
+            if (new Date().getTime() - lastRefreshTime.getTime() > 24 * 60 * 60000l) { /* one day */
+                Log.d(TAG, "It is time to force a refresh of beacon data");
+                beaconData.sync(this);
                 lastRefreshTime = new Date();
             }
         }
     }
 
-
     @Override
     public void didEnterRegion(Region region) {
-        // In this example, this class sends a notification to the user whenever an iBeacon
+        // In this example, this class sends a notification to the user whenever an beacon
         // matching a Region (defined above) are first seen.
-        Log.d(TAG, "did enter region.");
-        if (!haveDetectedIBeaconsSinceBoot) {
+        Log.d(TAG, "did enter region: " + region);
+        if (!haveDetectedBeaconsSinceBoot) {
             Log.d(TAG, "auto launching MainActivity");
 
-            // The very first time since boot that we detect an iBeacon, we launch the
+            // The very first time since boot that we detect an beacon, we launch the
             // MainActivity
             Intent intent = new Intent(this, MainActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
@@ -80,9 +88,9 @@ public class AndroidProximityKitReferenceApplication extends Application impleme
             // to keep multiple copies of this activity from getting created if the user has
             // already manually launched the app.
             this.startActivity(intent);
-            haveDetectedIBeaconsSinceBoot = true;
+            haveDetectedBeaconsSinceBoot = true;
         } else {
-            // If we have already seen iBeacons and launched the MainActivity before, we simply
+            // If we have already seen beacons and launched the MainActivity before, we simply
             // send a notification to the user on subsequent detections.
             Log.d(TAG, "Sending notification.");
             sendNotification();
@@ -103,7 +111,7 @@ public class AndroidProximityKitReferenceApplication extends Application impleme
         NotificationCompat.Builder builder =
                 new NotificationCompat.Builder(this)
                         .setContentTitle("Proximity Kit Reference Application")
-                        .setContentText("An iBeacon is nearby.")
+                        .setContentText("An beacon is nearby.")
                         .setSmallIcon(R.drawable.ic_launcher);
 
         NotificationManager notificationManager =
@@ -114,6 +122,4 @@ public class AndroidProximityKitReferenceApplication extends Application impleme
     public void setMainActivity(MainActivity mainActivity) {
         this.mainActivity = mainActivity;
     }
-
-
 }

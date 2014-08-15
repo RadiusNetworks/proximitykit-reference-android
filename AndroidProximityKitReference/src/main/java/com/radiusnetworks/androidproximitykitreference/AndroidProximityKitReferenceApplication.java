5bf52d499b5f8e3b302fedd1fcbe82f0cd555c8c
Aaron Kromer <aaron.kromer@gmail.com>
2014-08-14 22:16:53 -0400

    Use proximitykit-android 0.1.0.beta2
    - Update API calls with changes in beta2
    - Include Geofence support using Google Play services
    - Remove custom resources path in the gradle to use the default
      location; simplifying the setup for the project
    - Bump the minimum SDK per requirements via the new library

--- AndroidProximityKitReference/src/main/java/com/radiusnetworks/androidproximitykitreference/AndroidProximityKitReferenceApplication.java
+++ AndroidProximityKitReference/src/main/java/com/radiusnetworks/androidproximitykitreference/AndroidProximityKitReferenceApplication.java
@@ -7,26 +7,31 @@ import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.radiusnetworks.proximity.ProximityKitGeofenceNotifier;
+import com.radiusnetworks.proximity.ProximityKitGeofenceRegion;
 import com.radiusnetworks.proximity.ProximityKitManager;
 import com.radiusnetworks.proximity.ProximityKitMonitorNotifier;
 import com.radiusnetworks.proximity.ProximityKitRangeNotifier;
-import com.radiusnetworks.proximity.ProximityKitRegion;
 import com.radiusnetworks.proximity.ProximityKitSyncNotifier;
+import com.radiusnetworks.proximity.geofence.GooglePlayServicesException;
 
 import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.BeaconData;
-import org.altbeacon.beacon.BeaconManager;
-import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.Region;
 import org.altbeacon.beacon.client.DataProviderException;
 
 import java.util.Date;
 
 /**
- * Created by dyoung on 4/2/14.
- *
- * Note:  the kit associated with this
  */
-public class AndroidProximityKitReferenceApplication extends Application implements ProximityKitMonitorNotifier, ProximityKitRangeNotifier, ProximityKitSyncNotifier {
+public class AndroidProximityKitReferenceApplication
+        extends     Application
+        implements  ProximityKitMonitorNotifier,
+                    ProximityKitRangeNotifier,
+                    ProximityKitSyncNotifier,
+                    ProximityKitGeofenceNotifier {
 
     public static final String TAG = "AndroidProximityKitReferenceApplication";
     Date lastRefreshTime = new Date();
@@ -34,6 +39,11 @@ public class AndroidProximityKitReferenceApplication extends Application impleme
     private MainActivity mainActivity = null;
 
     @Override
+    /**
+     * @see <a href="https://developer.android.com/google/play-services/setup.html">
+     *          Setup Google Play services
+     *      </a>
+     */
     public void onCreate() {
         super.onCreate();
         Log.d(TAG, "onCreate() called");
@@ -41,12 +51,21 @@ public class AndroidProximityKitReferenceApplication extends Application impleme
         ProximityKitManager pkManager = ProximityKitManager.getInstanceForApplication(this);
 
         /* ----- begin code only for testing ---- */
-        pkManager.getBeaconManager().setDebug(true);
+        pkManager.debugOn();
         /* ----- end code only for testing ------ */
+
+        if (servicesConnected()) {
+            try {
+                pkManager.enableGeofences();
+            } catch (GooglePlayServicesException e) {
+                Log.e(TAG, e.getMessage());
+            }
+        }
 
         pkManager.setProximityKitSyncNotifier(this);
         pkManager.setProximityKitMonitorNotifier(this);
         pkManager.setProximityKitRangeNotifier(this);
+        pkManager.setProximityKitGeofenceNotifier(this);
         pkManager.start();
     }
 
@@ -57,7 +76,7 @@ public class AndroidProximityKitReferenceApplication extends Application impleme
 
     @Override
     public void didFailSync(Exception e) {
-        Log.d(TAG, "didFailSync() called with exception: "+e);
+        Log.d(TAG, "didFailSync() called with exception: " + e);
     }
 
     @Override
@@ -87,7 +106,7 @@ public class AndroidProximityKitReferenceApplication extends Application impleme
     }
 
     @Override
-    public void didEnterRegion(ProximityKitRegion region) {
+    public void didEnterRegion(Region region) {
         // In this example, this class sends a notification to the user whenever an beacon
         // matching a Region (defined above) are first seen.
         Log.d(TAG, "did enter region: " + region);
@@ -112,13 +131,13 @@ public class AndroidProximityKitReferenceApplication extends Application impleme
     }
 
     @Override
-    public void didExitRegion(ProximityKitRegion region) {
-        Log.d(TAG, "didExitRegion called with region: "+region);
+    public void didExitRegion(Region region) {
+        Log.d(TAG, "didExitRegion called with region: " + region);
     }
 
     @Override
-    public void didDetermineStateForRegion(int i, ProximityKitRegion region) {
-        Log.d(TAG, "didDeterineStateForRegion called with region: "+region);
+    public void didDetermineStateForRegion(int i, Region region) {
+        Log.d(TAG, "didDeterineStateForRegion called with region: " + region);
     }
 
     private void sendNotification() {
@@ -135,5 +154,43 @@ public class AndroidProximityKitReferenceApplication extends Application impleme
 
     public void setMainActivity(MainActivity mainActivity) {
         this.mainActivity = mainActivity;
+    }
+
+    @Override
+    public void didEnterGeofence(ProximityKitGeofenceRegion region) {
+        Log.d(TAG, "didEnterGeofenceRegion called with region: " + region);
+    }
+
+    @Override
+    public void didExitGeofence(ProximityKitGeofenceRegion region) {
+        Log.d(TAG, "didExitGeofenceRegion called with region: " + region);
+    }
+
+    @Override
+    public void didDetermineStateForGeofence(int i, ProximityKitGeofenceRegion region) {
+        Log.d(TAG, "didDeterineStateForGeofence called with region: " + region);
+    }
+
+    /**
+     * Verify that Google Play services is available before making a request.
+     *
+     * @see <a href="https://developer.android.com/google/play-services/setup.html">
+     *          Setup Google Play services
+     *      </a>
+     * @return true if Google Play services is available, otherwise false
+     */
+    private boolean servicesConnected() {
+        // Check that Google Play services is available
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+
+        if (ConnectionResult.SUCCESS != resultCode) {
+            Log.w(TAG, GooglePlayServicesUtil.getErrorString(resultCode));
+            GooglePlayServicesUtil.showErrorNotification(resultCode, this);
+
+            return false;
+        }
+
+        Log.d(TAG, "Google Play services available");
+        return true;
     }
 }
